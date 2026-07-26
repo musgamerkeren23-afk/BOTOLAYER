9ab150c6206980f96e5fb78565b4aea5de9fbc30
H4ll0 Creator Scripter <musgamerkeren23@gmail.com>
2026-07-26 09:38:32 +0700

    Update BotPlayerPopup.cpp

--- BOTPLAYER/src/BotPlayerPopup.cpp
+++ BOTPLAYER/src/BotPlayerPopup.cpp
@@ -5,7 +5,7 @@ extern GeneticBot g_geneticBot;
 
 BotPlayerPopup* BotPlayerPopup::create() {
     auto ret = new BotPlayerPopup();
-    if (ret->initAnchored(360.f, 260.f)) {
+    if (ret->init()) {
         ret->autorelease();
         return ret;
     }
@@ -13,11 +13,13 @@ BotPlayerPopup* BotPlayerPopup::create() {
     return nullptr;
 }
 
-bool BotPlayerPopup::setup() {
+bool BotPlayerPopup::init() {
+    if (!Popup::init(POPUP_WIDTH, POPUP_HEIGHT)) return false;
+
     this->setTitle("BOTPLAYER");
 
     m_tabMenu = CCMenu::create();
-    m_tabMenu->setPosition({ m_size.width / 2, m_size.height - 45.f });
+    m_tabMenu->setPosition({ POPUP_WIDTH / 2, POPUP_HEIGHT - 45.f });
 
     auto manualBtnSpr = ButtonSprite::create("Manual");
     auto manualBtn = CCMenuItemSpriteExtra::create(
@@ -56,7 +58,7 @@ CCLayer* BotPlayerPopup::buildManualTab() {
     layer->setPosition({ 0.f, 0.f });
 
     auto menu = CCMenu::create();
-    menu->setPosition({ m_size.width / 2, m_size.height / 2 - 20.f });
+    menu->setPosition({ POPUP_WIDTH / 2, POPUP_HEIGHT / 2 - 20.f });
 
     auto recordBtnSpr = ButtonSprite::create("Record / Stop");
     auto recordBtn = CCMenuItemSpriteExtra::create(
@@ -76,7 +78,7 @@ CCLayer* BotPlayerPopup::buildManualTab() {
 
     m_manualStatusLabel = CCLabelBMFont::create("Status: Idle", "bigFont.fnt");
     m_manualStatusLabel->setScale(0.4f);
-    m_manualStatusLabel->setPosition({ m_size.width / 2, m_size.height / 2 - 70.f });
+    m_manualStatusLabel->setPosition({ POPUP_WIDTH / 2, POPUP_HEIGHT / 2 - 70.f });
     layer->addChild(m_manualStatusLabel);
 
     return layer;
@@ -87,7 +89,7 @@ CCLayer* BotPlayerPopup::buildAutomaticTab() {
     layer->setPosition({ 0.f, 0.f });
 
     auto menu = CCMenu::create();
-    menu->setPosition({ m_size.width / 2, m_size.height / 2 - 20.f });
+    menu->setPosition({ POPUP_WIDTH / 2, POPUP_HEIGHT / 2 - 20.f });
 
     auto autoBtnSpr = CCSprite::createWithSpriteFrameName("GJ_playBtn_001.png");
     auto autoBtn = CCMenuItemSpriteExtra::create(
@@ -100,7 +102,7 @@ CCLayer* BotPlayerPopup::buildAutomaticTab() {
 
     m_autoStatusLabel = CCLabelBMFont::create("Gen: 0 | Best: 0%", "bigFont.fnt");
     m_autoStatusLabel->setScale(0.4f);
-    m_autoStatusLabel->setPosition({ m_size.width / 2, m_size.height / 2 - 70.f });
+    m_autoStatusLabel->setPosition({ POPUP_WIDTH / 2, POPUP_HEIGHT / 2 - 70.f });
     layer->addChild(m_autoStatusLabel);
 
     return layer;
@@ -128,7 +130,7 @@ void BotPlayerPopup::onRecordToggle(CCObject*) {
 
 void BotPlayerPopup::onPlayback(CCObject*) {
     if (g_manualTab.getMacro().empty()) {
-        Notification::create("macro ini kosong!", NotificationIcon::Error)->show();
+        FLAlertLayer::create("Oops", "macro ini kosong!", "OK")->show();
         return;
     }
     g_manualTab.startPlayback();
